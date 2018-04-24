da275511275f8faa25d085aecf867047f4698806
Christopher Lee <chmalee@ucsc.edu>
2018-04-24 15:09:32 -0700

    getline for loop with no logical AND or OR operators

---
+++ ch2/exer2-2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+/* Same as exercise 1-16, but with no && or || in getLine for loop. */
+
+int getLine(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main()
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+    while ((len = getLine(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0)
+        printf("longest length: %d, line: %s", max, longest);
+    return 0;
+}
+
+/* getline: read a line into s, return length */
+int getLine(char s[], int lim)
+{
+    int c, i;
+    for (i = 0; i < lim-1; ++i) {
+        if ((c=getchar()) != EOF) {
+            if (c != '\n')
+                s[i] = c;
+            else break;
+        } else {
+            break;
+        }
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
