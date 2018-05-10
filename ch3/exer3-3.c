69005d575a295d005f7326c73d0f51aaa1f18b79
Christopher Lee <chmalee@ucsc.edu>
2018-05-10 13:40:45 -0700

    working a-z expansion

--- ch3/exer3-3.c
+++ ch3/exer3-3.c
@@ -7,38 +7,42 @@ void expand (char s1[], char s2[]);
 /* expands shorthand like a-z into abc..xyz */
 void expand (char s1[], char s2[])
 {
-    int i, temp, rStart,rEnd,seps;
-    seps = 0;
-    for (i = 0; i != 0; i++) {
-        switch s[i]:
-            case isdigit(s[i]):
-                break;
-            case '-':
-                seps++;
-                break;
-            default:
-                if 
-        if (isdigit(s1[i])) {
-            temp = rEnd;
-            rEnd = rStart;
-            rStart = temp;
+    int i, j, temp, rStart, rEnd, seps, len;
+    rStart = rEnd = -1;
+    seps = len = 0;
+    for (i = 0; s1[i] != '\0'; i++) {
+        printf("s1[%d] = %c\n", i, s1[i]);
+        if (s1[i] == '-')
+            continue;
+        else if (rStart == -1) {
+            printf("setting rStart to %c\n", s1[i]);
+            rStart = s1[i];
+        } else if (rEnd == -1) {
+            printf("setting rEnd to %c\n", s1[i]);
+            rEnd = s1[i];
         }
-    printf("%c\n", rStart);
-    printf("%c\n", rEnd);
-    for (i = 0; i <= rEnd-rStart; i++)
-        s2[i] = rStart + i;
-    s2[i] = '\0';
+        if ((rStart > -1) && (rEnd > -1)) {
+            printf("%c\n", rStart);
+            printf("%c\n", rEnd);
+            for (j = 0; j <= rEnd-rStart; j++) {
+                s2[j] = rStart + j;
+                ++len;
+            }
+            rStart = rEnd = -1;
+        }
+    }
+    s2[len] = '\0';
 }
 
 int main()
 {
     char s1[] = "a-z";
-    char s2[] = "a-z0-9";
+    //char s2[] = "a-z0-9";
     char s3[36]; //36 is a-z + 10 for 0-9
-    char s4[36];
+    //char s4[36];
     
     expand(s1,s3);
-    expand(s2,s4);
+    //expand(s2,s4);
     printf("%s expanded: %s\n", s1, s3);
-    printf("%s expanded: %s\n", s2, s4);
+    //printf("%s expanded: %s\n", s2, s4);
 }
