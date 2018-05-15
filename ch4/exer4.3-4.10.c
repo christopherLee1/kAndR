87133688c0988126fa2cd6b870cc584ffae776e3
Christopher Lee <chmalee@ucsc.edu>
2018-05-15 14:54:58 -0700

    exercises 4-7 through 4-9 done

--- ch4/exer4.3-4.10.c
+++ ch4/exer4.3-4.10.c
@@ -154,6 +154,7 @@ void printAll(void)
 
 int getch(void);
 void ungetch(int);
+void ungets(char []);
 
 /* getop: get next operator or numeric operand */
 int getop(char s[])
@@ -187,18 +188,31 @@ int getop(char s[])
 
 #define BUFSIZE 100
 
-char buf[BUFSIZE]; // buffer for ungetch
-int bufp = 0; // next free position in buf
+// buffer for ungetch, ints to handle EOF correctly, no need for array if max is one putback 
+int buf = EOF;
 
-int getch(void) /* get a (possibly pushed back) character */
+/* get a (possibly pushed back) character */
+int getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    int temp;
+    if (buf != EOF) {
+        temp = buf;
+        buf = EOF;
+    } else
+        temp = getchar();
+    return temp;
 }
 
-void ungetch(int c) /* push character back on input */
+/* push character back on input */
+void ungetch(int c)
 {
-    if (bufp >= BUFSIZE)
-        printf("ungetch: too many characters\n");
-    else
-        buf[bufp++] = c;
+    buf = c;
+}
+
+/* pushes entire string s  back on input */
+void ungets(char s[])
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+        ungetch(s[i]);
 }
