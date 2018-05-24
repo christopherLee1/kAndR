bc5f6da4895f4baab41cff069466fac82e441ac5
Christopher Lee <chmalee@ucsc.edu>
2018-05-24 14:27:33 -0700

    atoi with pointers

--- ch5/exer5-6.c
+++ ch5/exer5-6.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/* atoi with pointers */
+int atoi1(char *s)
+{
+int n, sign;
+for (; isspace(*s); s++)
+    ;
+sign = (*s == '-') ? -1 : 1;
+if (*s == '+' || *s == '-')
+    s++;
+for (n = 0; isdigit(*s); s++)
+    n = 10 * n + (*s - '0');
+return sign *n;
+}
+
 /* strindex: return index of t in s, -1 if none */
 int strindex(char *s, char *t)
 {
@@ -25,7 +39,11 @@ return -1;
 
 int main()
 {
-    char *s = "index 0 negative 1";
-    char *t = "negative";
-    printf("strindex(%s,%s): %d, should be 8\n", s, t, strindex(s,t));
+    // strindex test
+    //char *s = "index 0 negative 1";
+    //char *t = "negative";
+    //printf("strindex(%s,%s): %d, should be 8\n", s, t, strindex(s,t));
+
+    char *s = "123456";
+    printf("atoi(%s) = %d\n", s, atoi1(s));
 }
