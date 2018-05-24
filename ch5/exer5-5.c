468875a09c5a6248408f6a3216a7968cc9a8fc0b
Christopher Lee <chmalee@ucsc.edu>
2018-05-24 13:38:21 -0700

    strncmp done, finished exercise 5-5

--- ch5/exer5-5.c
+++ ch5/exer5-5.c
@@ -13,7 +13,6 @@ char *strncpyMy(char *s, char *t, int n)
 {
 if (n > strlenMy(t))
     n = strlenMy(t);
-
 int i = 0;
 char *r = s;
 while (i < n)
@@ -27,6 +26,8 @@ return r-n;
 /* copy at most n chars of t onto s */
 char *strncatMy(char *s, char *t, int n)
 {
+if (n > strlenMy(t))
+    n = strlenMy(t);
 int i = strlenMy(s);
 int j = 0;
 char *r = s;
@@ -43,32 +44,21 @@ return r-i-n;
 /* compare at most n chars of s with t */
 int strncmpMy(char *s, char *t, int n)
 {
-return 0;
-}
-
-/* return 1 if t is at the end s, 0 otherwise */
-int strendMy(char *s, char *t)
+if (n > strlenMy(t))
+    n = strlenMy(t);
+int i = 0;
+for (; *s == *t && i < n; s++, t++, i++)
 {
-int i = 0, j = 0;
-i = strlenMy(t);
-j = strlenMy(s);
-if (j < i)
-    return 0;
-s += j - i;
-while (*t != '\0')
-{
-    if (*s != *t)
+    if (*(t+1) == '\0')
         return 0;
-    t++;
-    s++;
 }
-return 1;
+return *s - *t;
 }
 
 int main()
 {
     char *r1, *r2;
-    int r3, r4;
+    int r3, r4, r5;
     char s1[] = "hello oh";
     char t1[] = "oh";
 
@@ -78,8 +68,11 @@ int main()
     char s3[] = "hello oh";
     char t3[] = "oh";
 
-    char s4[] = "hello oh";
-    char t4[] = "he";
+    char s4[] = "x1ello oh";
+    char t4[] = "x1el";
+
+    char s5[] = "hello oh";
+    char t5[] = "ae";
 
     printf("strncpyMy(%s, %s, 2): ", s1, t1);
     r1 = strncpyMy(s1, t1, 3); // "ohllo oh"
@@ -88,9 +81,11 @@ int main()
     printf("strncat(%s, %s, 2): ", s2, t2);
     r2 = strncatMy(s2, t2, 2); // "hello ohoh"
     printf("%s\n", r2);
-    //r3 = strncmpMy(s3, t3, 2); // " return 0
-    //r4 = strncmpMy(s4, t4, 2); // " return 1
+    r3 = strncmpMy(s3, t3, 2); // " return -number
+    r4 = strncmpMy(s4, t4, 2); // " return 0
+    r5 = strncmpMy(s5, t5, 4); // " return +number
     
-    //printf("strncmp(%s, %s, 2): %s\n", s3, t3, strncmp(s3, t3, 2));
-    //printf("strncmp(%s, %s, 2): %s\n", s3, t3, strncmp(s4, t4, 2));
+    printf("strncmp(%s, %s, 2): %d\n", s3, t3, r3);
+    printf("strncmp(%s, %s, 2): %d\n", s4, t4, r4);
+    printf("strncmp(%s, %s, 2): %d\n", s5, t5, r5);
 }
