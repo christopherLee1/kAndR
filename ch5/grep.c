bb2f3950379ae262f9ccccc24daa2f84839aae40
Christopher Lee <chmalee@ucsc.edu>
2018-06-07 13:42:02 -0700

    grep with -n and -v options

--- ch5/grep.c
+++ ch5/grep.c
@@ -15,19 +15,44 @@ if (c == '\n')
 return p-s;
 }
 
-/* grep: print lines taht match pattern from 1st arg */
+/* grep: print lines that match pattern from 1st arg
+    invert -n for line number of match and
+            -v for inversion of match */
 int main(int argc, char *argv[])
 {
 char line[MAXLINE];
-int found = 0;
-if (argc != 2)
-    printf("Usage: ./grep pattern\n");
-else {
-    while (getLine(line, MAXLINE) > 0)
-        if (strstr(line, argv[1]) != NULL) {
-            printf("%s", line ); // prints found line as it's found, doesn't wait for all input
+long lineno = 0;
+int c, invert = 0, number = 0, found = 0;
+
+while (--argc > 0 && (*++argv)[0] == '-') {
+    while ((c = *++argv[0])) {
+        switch(c) {
+            case 'v':
+                invert = 1;
+                break;
+            case 'n':
+                number = 1;
+                break;
+            default:
+                printf("grep: illegal option %c\n", c);
+                argc = 0;
+                found = -1;
+                break;
+        }
+    }
+}
+if (argc != 1) {
+    printf("Usage: ./grep -n -v pattern\n");
+} else {
+    while (getLine(line, MAXLINE) > 0) {
+        lineno++;
+        if ((strstr(line, *argv) != NULL) != invert) {
+            if (number)
+                printf("%ld:", lineno);
+            printf("%s", line );
             found++;
         }
+    }
 }
 return found;
 }
