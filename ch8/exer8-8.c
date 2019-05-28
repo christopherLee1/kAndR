4a181522a4024b29d24e3950107507bd0f9a43c2
Christopher Lee <chmalee@ucsc.edu>
2019-05-28 08:34:48 -0700

    last exercise?

---
+++ ch8/exer8-8.c
@@ -0,0 +1,14 @@
+#include "ch8Code.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+/*  test of _bfree */
+{
+int i;
+char **nameArray = _calloc(5, sizeof(char) * 6); // 5 array elements that can hold "chris" plus null
+for (i = 0; i < 5; i++)
+    nameArray[i] = "chris";
+for (i = 0; i < 5; i++)
+    printf("nameArray[%d] = %s\n", i, nameArray[i]);
+_free(&nameArray);
+}
